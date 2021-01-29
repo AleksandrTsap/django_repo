0ba0f2911e02b61bc1ac260ae81e5313aad3e301
Aleksandr <tsap97@gmail.com>
2021-01-29 15:34:13 +0300

    Auto stash before merge of "main" and "lesson2"

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,13 +1,48 @@
 from django.shortcuts import render
+import datetime
 
-
-# Create your views here.
 
 def index(request):
-    return render(request, 'mainapp/index.html')
+    contex = {
+        'page_title': 'главная',
+    }
+    return render(request, 'mainapp/index.html', contex)
+
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    menu = [{'category': 'все'},
+            {'category': 'дом'},
+            {'category': 'офис'},
+            {'category': 'модер'},
+            {'category': 'классика'}, ]
+    contex = {
+        'page_title': 'продукты',
+        'menu': menu,
+    }
+    return render(request, 'mainapp/products.html', contex)
+
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    locations = [
+        {
+            'city': 'Москва',
+            'phone': 88003331122,
+            'email': 'msk@mail.ru',
+            'address': 'В пределах МКАД'
+        },
+        {
+            'city': 'Cанкт-Петербург',
+            'phone': 88123331111,
+            'email': 'spb@mail.ru',
+            'address': 'В пределах КАД'},
+        {
+            'city': 'Иркутск',
+            'phone': 830123333333,
+            'email': 'irk@mail.ru',
+            'address': 'В пределах центра'}
+    ]
+    contex = {
+        'page_title': 'контакты',
+        'locations': locations,
+    }
+    return render(request, 'mainapp/contact.html', contex)
